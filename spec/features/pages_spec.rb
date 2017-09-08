558fac9770fadf3ee672758a310474c18bfb52b8
c-Nosetab <cpbateson@gmail.com>
2017-09-07 20:57:05 -0500

    added some tests, still more to do

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
-Rspec.describe 'Clicking the home button', :type => :feature do
-  visit '/about'
-  find_link('Home').click
-  expect(page).to have_content 'Connect Customers in Need with Veterans in Skilled Trades'
+RSpec.describe 'Clicking navbar links', :type => :feature do
+
+  it 'goes to the homepage' do
+    visit '/about'
+    find_link('Home').click
+    expect(page).to have_content 'Connect Customers in Need with Veterans in Skilled Trades'
+  end
+
+  it 'goes to the how page' do
+    visit '/'
+    find_link('How').click
+    expect(page).to have_content 'How It Works:'
+  end
+
+
 end
